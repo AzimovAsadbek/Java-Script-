c7abafbed95d52030c1e3408f260845664d037c3
Asadbe <asadbekazimov0629@gmail.com>
2023-10-04 16:43:26 +0500

    Proto and Prototype

---
+++ proto and prototype/script.js
@@ -0,0 +1,28 @@
+// Proto and Prototype
+// let part1 = {
+//   name: "Tom",
+// };
+// let part2 = {
+//   name: "john",
+//   age: 20,
+// };
+// Object.assign(part1, part2);
+// console.log(part1.age);
+// __proto__
+// part1.__proto__ = part2;
+// console.log(part1);
+
+// prototype
+// let ar = [1, 2, 3, 4, 5, 6, 7, 8];
+// Array.prototype.removeEnens = function () {
+//   return this.filter((v) => v % 2);
+// };
+// console.log(ar);
+// console.log(ar.removeEnens());
+
+// let string = new String("hello")
+// String.prototype.sayHi = function (name){
+//     return `Hi ${name}`
+// }
+// console.log(string.sayHi("Tom"));
+
